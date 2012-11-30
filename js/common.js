d7b98ef2b5f772323adf581407889fc8e5906b0c
Artur <morozarturv@gmail.com>
2012-11-30 16:21:59 +0200

    scroll 90%

--- js/common.js
+++ js/common.js
@@ -1,12 +1,71 @@
 $(document).ready(function () {
 
 //nav scroll
-jQuery('.nav-scroll button').click(function() {
-	str = jQuery(this).val();
-	jQuery.scrollTo("#"+str, 500);
+$('.nav-scroll button').click(function() {
+	var str = $(this).val();
+	$(window).scrollTo("."+str, 800);
 });
-
-
+//add and remove class for nav
+$(window).scroll(function () {
+	var height_1 = $('.header-out').height();
+	var height_2 = $('.fast').height();
+	var height_3 = $('.what-we-do').height();
+	var height_4 = $('.about').height();
+	var height_5 = $('.why-us').height();
+	var height_6 = $('.reviews').height();
+	var height_7 = $('.samples').height();
+	if ($(this).scrollTop() < height_1) {
+		$('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__1').addClass('active');        
+  };
+	if ($(this).scrollTop() >= height_1) {
+    $('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__2').addClass('active');
+  };
+  if ($(this).scrollTop() >= height_1+height_2) {
+    $('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__3').addClass('active');
+  };
+  if ($(this).scrollTop() >= height_1+height_2+height_3) {
+    $('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__4').addClass('active');
+  };
+  if ($(this).scrollTop() >= height_1+height_2+height_3+height_4) {
+    $('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__5').addClass('active');
+  };
+  if ($(this).scrollTop() >= height_1+height_2+height_3+height_4+height_5) {
+    $('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__6').addClass('active');
+  };
+  if ($(this).scrollTop() >= height_1+height_2+height_3+height_4+height_5+height_6) {
+    $('.nav-scroll button').removeClass('active');
+    $('.nav-scroll__7').addClass('active');
+  };
+});
+//nav-scroll on scroll
+//var running = 1;
+$('body').bind('mousewheel', function(event, delta) {  
+	//if (running != 0) {
+		//var running = 0;		
+	  if (delta < 0) { 
+	  	var btn_act1 = $('.nav-scroll button.active').parent().next().children('button').val();	
+	  	$(window).scrollTo("."+btn_act1, 800, function() { 
+	  		//var running = 1;        
+			});
+	  } 
+	  else { 
+	  	var btn_act2 = $('.nav-scroll button.active').parent().prev().children('button').val();	
+			$(window).scrollTo("."+btn_act2, 800, function() { 
+	  		//var running = 1;        
+			});
+	 // } 	  
+  }
+  //else {
+  //	alert('fsd');
+  //}
+  return false; 
+});
 
 //hide all popups
 $('.transparent').click(function() {
@@ -42,20 +101,65 @@ $('.pay__select li').click(function() {
 });
 
 //datepicker
-$('.datepicker').datepicker({
-	onSelect: function(date) { 
-		$('.calendar__date').html(date+'<i></i>');
-		$('.calendar').addClass('selected');
-		$('.datepicker').fadeOut();
-		$('.transparent').hide();
-		$('.calendar').removeClass('z-index');
+if ($('.datepicker').length>0) {
+	$('.datepicker').datepicker({
+		onSelect: function(date) { 
+			$('.calendar__date').html(date+'<i></i>');
+			$('.calendar').addClass('selected');
+			$('.datepicker').fadeOut();
+			$('.transparent').hide();
+			$('.calendar').removeClass('z-index');
+		}
+	});
+	$('.calendar__date').click(function() {
+		$('.transparent').show();
+		$('.datepicker').fadeIn();
+		$('.calendar').addClass('z-index');
+	});
+};
+
+});
+
+
+
+
+
+function mouseWheel(element, callback, context) {
+//Mouse wheel event handler
+var wheelHandler = function (event) {
+	var delta = 0;
+	event = event || window.event;
+	if (event.wheelDelta) { //IE
+		delta = event.wheelDelta/120;
+	} else if (event.detail) { //firefox
+		delta = -event.detail/3;
 	}
-});
-$('.calendar__date').click(function() {
-	$('.transparent').show();
-	$('.datepicker').fadeIn();
-	$('.calendar').addClass('z-index');
-});
+	
+	if (event.preventDefault) {
+		event.preventDefault();
+	}
+	event.returnValue = false;
+	if (delta) {
+		callback.call(context || this, event, delta); //callback function
+	}
+}
 
-
-});
+if (element.addEventListener) { //For firefox
+	element.addEventListener('DOMMouseScroll', wheelHandler, false); //Mouse wheel initialization
+} else if (element.attachEvent) { //For IE
+	element.attachEvent('onmousewheel',wheelHandler); //Mouse wheel initialization
+}
+return wheelHandler;
+}
+function removeMouseWheel(element, eventHandler) {
+if (element.removeEventListener) {
+	element.removeEventListener('DOMMouseScroll',eventHandler,false);
+} else if (element.detachEvent) {
+	element.detachEvent('onmousewheel',eventHandler);
+}
+}
+/*Example usage - First make a callback function...*/
+//Within the body element, create a HTML div with id="test" and try the following:
+function display(event, delta) {
+alert(delta);
+}
